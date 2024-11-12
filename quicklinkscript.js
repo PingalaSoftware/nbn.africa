53a5e552a3e64e8b0fdd7d011860b4f5ba4e1521
bharath-pingala <bharath@pingalasoftware.com>
2024-11-12 18:25:03 +0530

    innitial commit

---
+++ quicklinkscript.js
@@ -0,0 +1,41 @@
+document.getElementById('nav-about').addEventListener('click', function () {
+    document
+        .getElementById('about-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
+
+document
+    .getElementById('nav-key-features')
+    .addEventListener('click', function () {
+        document
+            .getElementById('key-features-section')
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+
+document
+    .getElementById('nav-hOW-it-works')
+    .addEventListener('click', function () {
+        document
+            .getElementById('hOW-it-works-section')
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+
+document
+    .getElementById('nav-get-involved')
+    .addEventListener('click', function () {
+        document
+            .getElementById('get-involved-section')
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+
+document.getElementById('nav-faq').addEventListener('click', function () {
+    document
+        .getElementById('faq-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('nav-contact').addEventListener('click', function () {
+    document
+        .getElementById('contact-section')
+        .scrollIntoView({ behavior: 'smooth' });
+});
